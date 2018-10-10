1ea5b5f0c2e772db65acd26ab43bd03760f3c277
Eduard Panteleev <eduardvipgroup@ya.ru>
2018-10-10 22:43:26 +1000

    3 дз

--- request.js
+++ request.js
@@ -1,7 +1,8 @@
-const request = require('request');
+const request = require('request'); // не utf-8
 const cheerio = require('cheerio');
+const needle = require('needle');
 
-request('http://www.komcity.ru/news/', function (error, response, html) {
+needle('http://www.komcity.ru/news/', function (error, response, html) {
     if (!error && response.statusCode === 200) {
         const $ = cheerio.load(html);
         const cols = $('#ad');
@@ -9,5 +10,14 @@ request('http://www.komcity.ru/news/', function (error, response, html) {
         cols.each(function () {
             console.dir($(this).text());
         });
+    }
+});
+needle.get('https://habr.com', function(error, response) {
+    if (!error && response.statusCode === 200) {
+        const $ = cheerio.load(response.body);
+        const el = $('h2 a');
+        el.each((idx, item) => {
+            console.log((idx + 1) + '.', $(item).text());
+    })
     }
 });
